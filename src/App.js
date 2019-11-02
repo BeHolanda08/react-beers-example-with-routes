dd64836ff67dfdb376858356f53e0b1a468a2565
Henrique Guazzelli Mendes <henriqueguazzellimendes@Henriques-MBP.localdomain>
2019-11-02 12:03:24 -0300

    example with routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Beers from './components/pages/Beers/Beers';
+import Home from './components/pages/Home/Home';
+import BeerDetail from './components/pages/BeerDetail/BeerDetail';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h1>Iron BEERS!!!</h1>
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/beers">Beers</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/beers' render={(props) => <Beers {...props} minhaProp="kdjaskldj" />} />
+        <Route exact path="/beers/:id" component={BeerDetail}/>
+      </Switch>
     </div>
   );
 }
